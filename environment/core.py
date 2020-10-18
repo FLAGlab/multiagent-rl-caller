b457cd26077980eddf688e7c67e29d0d60a40091
jssosa10 <js.sosa10@uniandes.edu.co>
2020-10-18 02:52:31 +0000

    more core elements

--- environment/core.py
+++ environment/core.py
@@ -9,6 +9,9 @@ class Survey():
         self.age = age
         self.is_urban = urban
 
+    def do_complete(self):
+        self.complete = True
+
     def __eq__(self, other):
         if isinstance(other, self.__class__):
             return self.__dict__ == other.__dict__
@@ -31,6 +34,27 @@ class Study():
             survey = Survey(len(self.surveys), socioeconomic=socioeconomic, age=age, urban=urban)
             self.surveys = np.append(self.surveys, survey)
 
+    def get_surveys(self, socioeconomic, age, urban):
+        return [survey for survey in self.surveys
+                if survey.socioeconomic == socioeconomic
+                and survey.age == age
+                and survey.is_urban == urban
+                and not survey.complete]
+
+    def complete_survey(self, id):
+        for i in range(len(self.surveys)):
+            if self.surveys[i].id == id:
+                self.surveys[i].do_complete()
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
 
 class Agent():
     def __init__(self, id):
@@ -40,4 +64,8 @@ class Agent():
 class World():
     def __init__(self):
         self.num_studies = None
-        self.studies = None
+        self.studies = np.array([])
+
+    def add_study(self, study):
+        self.studies = np.append(self.studies, study)
+        self.num_studies = len(self.studies)
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,5 @@
 import unittest
-from environment.core import Survey, Study
+from environment.core import Survey, Study, World
 
 
 class TestSurvey(unittest.TestCase):
@@ -20,3 +20,32 @@ class TestStudy(unittest.TestCase):
         self.study.add_survey(2, 20, True)
         self.assertEqual(len(self.study.surveys), 1)
         self.assertEqual(self.study.surveys[-1], self.survey)
+
+    def test_get_surveys(self):
+        self.study.add_survey(2, 20, True)
+        self.study.add_survey(4, 23, True)
+        self.assertEqual(len(self.study.get_surveys(2, 20, True)), 1)
+        self.assertEqual(self.study.get_surveys(2, 20, True)[-1], self.survey)
+
+    def test_get_empty_surveys_subset(self):
+        self.study.add_survey(2, 20, True)
+        self.study.add_survey(4, 23, True)
+        self.assertEqual(len(self.study.get_surveys(4, 20, True)), 0)
+
+    def test_complete_survey(self):
+        self.study.add_survey(2, 20, True)
+        self.study.add_survey(4, 23, True)
+        self.study.complete_survey(0)
+        self.assertEqual(len(self.study.get_surveys(2, 20, True)), 0)
+
+
+class TestWorld(unittest.TestCase):
+
+    def setUp(self):
+        self.world = World()
+        self.study = Study(1, 3)
+
+    def test_add_study(self):
+        self.world.add_study(self.study)
+        self.assertEqual(self.world.num_studies, 1)
+        self.assertEqual(self.world.studies[-1], self.study)
